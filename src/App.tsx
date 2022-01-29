6220d56b17d562c9f7cfe0cde843bd7265170977
Takashi Suzuki <keshipink@gmail.com>
2022-01-29 23:36:50 +0900

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,49 @@
+import React, { useState } from 'react'
+
+type Todo = {
+  value: string
+  readonly id: number
+}
+
+function App() {
+  const [text, setText] = useState('')
+  const [todos, setTodos] = useState<Todo[]>([])
+
+  const handleOnSubmit = () => {
+    if (!text) return
+
+    const newTodo: Todo = {
+      value: text,
+      id: new Date().getTime(),
+    }
+
+    setTodos([newTodo, ...todos])
+    setText('')
+  }
+  const handleOnChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setText(e.target.value)
+  }
+
+  return (
+    <div>
+      <form onSubmit={(e) => {
+        e.preventDefault()
+        handleOnSubmit()
+      }}>
+        <input type="text" value={text} onChange={(e) => handleOnChange(e)} />
+        <input
+          type="submit"
+          value="追加"
+          onSubmit={handleOnSubmit}
+        />
+      </form>
+      <ul>
+        {todos.map((todo) => {
+          return <li key={todo.id}>{todo.value}</li>
+        })}
+      </ul>
+    </div>
+  )
+}
+
+export default App
